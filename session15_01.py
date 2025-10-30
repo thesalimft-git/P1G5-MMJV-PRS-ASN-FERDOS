5f8c8c805b0c22c287ec9b19d9731eb4d4437dec
sali mft <thesalimft@gmail.com>
2025-10-30 18:58:39 +0330

    ass session 15

---
+++ session15_01.py
@@ -0,0 +1,20 @@
+# - Create venv
+# python -m venv .venv
+
+
+# - Get the source of python
+# which python
+# python --version
+
+
+# - Activate venv in terminal
+# source .venv/Scrpits/active
+# deactivate
+
+
+# - PIP = Package Manager
+# pip install camelcase
+# pip uninstall camelcase
+# pip freeze
+# pip freeze > requirements.txt
+# pip install -r requirements.txt
