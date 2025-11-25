4908a98af9aaa14ab8c16bb66548978d1ee782ab
sali mft <thesalimft@gmail.com>
2025-11-25 19:49:28 +0330

    part 2

--- session22/bank_account_management.py
+++ session22/bank_account_management.py
@@ -4,9 +4,21 @@
 
 
 
-
 class BankAccountManagement:
-    def __init__(self, account):
-        self.account = account
+    def __init__(self, accounts:dict):
+        self.accounts = accounts
+        
+    def add_user(self, name:str, first_amount:float):
+        id = len(self.accounts) + 1
+        self.accounts.update({
+            id: {
+                    'owner': name, 
+                    'balance': first_amount, 
+                    'transactions': [
+                        {'time': '202 ???? ', type: 'deposit', 'amount': first_amount},
+                    ],
+                    'status': 'active'     
+                }
+        })
     
     
