aa5600e4a6a7d711098396db1c21fa28c4c24ac2
Fr2s <heidari.fe1997gmail.com>
2025-11-26 14:23:09 +0330

    session 22

---
+++ session22/main.py
@@ -0,0 +1,36 @@
+from data import accounts
+from bank_account_management import BankAccountManagement
+
+bam = BankAccountManagement(accounts)
+
+def show_menu():
+    print('\n\n\n------------------')
+    print('1- Show All')
+    print('2- Add user')
+    print('3- Transfer')
+    print('0- Exit')
+
+def add_user():
+    name = input('insert name: ')
+    first_amount = input('insert amount: ')
+    result = bam.add_user(name, first_amount)
+    if result['status'] == 'ok':
+        print(f"{name} is addedd successfully")
+        
+    if result['status'] == 'error':
+        print(result['msg'])    
+     
+def main():
+    while True:
+        show_menu()
+        command = input('select from menu: ')
+        match command:
+            case '1':
+                bam.show_info()
+            case '2':
+                add_user()
+            case '0':
+                break
+                
+        
+main()
